7c01194480a8b8f0a6ceb85088992c06b7931d4e
Alex Nico <a.nicolaidis@outlook.com>
2023-01-22 14:13:40 +1000

    Fixed Titles and Printed List to screen

--- src/components/FileBrowser.jsx
+++ src/components/FileBrowser.jsx
@@ -1,29 +1,29 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import { useState } from "react";
 
 function FileBrowser() {
+  const [reactionData, setReactionData] = useState(null);
 
-    let fileReader;
+  let fileReader;
 
+  const handleFileRead = (e) => {
+    const content = fileReader.result;
+    setReactionData(content.split("\n"));
+  };
 
-    const handleFileRead = (e) => {
-        const content = fileReader.result;
-        console.log(content.split('\n'))
-    }
+  const readData = (e) => {
+    fileReader = new FileReader();
+    fileReader.onloadend = handleFileRead;
+    fileReader.readAsText(e.target.files[0]);
+  };
 
-    const readData = (e) => {
-        fileReader = new FileReader();
-        fileReader.onloadend = handleFileRead;
-        fileReader.readAsText(e.target.files[0]);
-        
-    }
-
-    return (
-        <div>This is where you can search for files
-
-            <input onChange={readData} type="file" accept='.txt,.rc'/>
-
-        </div>
-    )
+  return (
+    <div>
+      This is where you can search for files
+      <input onChange={readData} type="file" accept=".txt,.rc,.rtf" />
+      <div>{reactionData}</div>
+    </div>
+  );
 }
 
 export default FileBrowser;
--- src/components/Reactions.jsx
+++ src/components/Reactions.jsx
@@ -1,71 +1,68 @@
 import React, { useState } from "react";
 
 function Reactions() {
+  const [fx, setFx] = useState(false);
+  const [fy, setFy] = useState(false);
+  const [fz, setFz] = useState(false);
+  const [mx, setMx] = useState(false);
+  const [my, setMy] = useState(false);
+  const [mz, setMz] = useState(false);
 
-    const [fx, setFx] = useState(false);
-    const [fy, setFy] = useState(false);
-    const [fz, setFz] = useState(false);
-    const [mx, setMx] = useState(false);
-    const [my, setMy] = useState(false);
-    const [mz, setMz] = useState(false);
-
-    
-    //keep track of the checkboxes
-    const handleCheckBox = (e) => {
-        switch(e.target.id){
-            case 'fx':
-                setFx(e.target.checked)
-                break;
-            case 'fy':
-                setFy(e.target.checked)
-                break;
-            case 'fz':
-                setFz(e.target.checked)
-                break;
-            case 'mx':
-                setMx(e.target.checked)
-                break;
-            case 'my':
-                setMy(e.target.checked)
-                break;
-            case 'mz':
-                setMz(e.target.checked)
-                break;
-        }
+  //keep track of the checkboxes
+  const handleCheckBox = (e) => {
+    switch (e.target.id) {
+      case "fx":
+        setFx(e.target.checked);
+        break;
+      case "fy":
+        setFy(e.target.checked);
+        break;
+      case "fz":
+        setFz(e.target.checked);
+        break;
+      case "mx":
+        setMx(e.target.checked);
+        break;
+      case "my":
+        setMy(e.target.checked);
+        break;
+      case "mz":
+        setMz(e.target.checked);
+        break;
     }
+  };
 
-    const selectedReactions = (e) => {
-        e.preventDefault()
-        console.log([fx,fy,fz,mx,my,mz])
-    }
+  const selectedReactions = (e) => {
+    e.preventDefault();
+    console.log([fx, fy, fz, mx, my, mz]);
+  };
 
-
-
-    return(
-        <div>
-            <form action="submit">
-                <div class='column'>
-                    <input onChange={handleCheckBox} type="checkbox" id="fx" name='fx'/>
-                    <label for='fy'>Fy</label>
-                    <input onChange={handleCheckBox} type="checkbox" id="fy" name='fy'/>
-                    <label for='fz'>Fz</label>
-                    <input onChange={handleCheckBox} type="checkbox" id="fz" name='fz'/>
-                    <label for='mx'>Mx</label>
-                </div>
-                <div>
-                    <input onChange={handleCheckBox} type="checkbox" id="mx" name='mx'/>
-                    <label for='my'>My</label>
-                    <input onChange={handleCheckBox}type="checkbox" id="my" name='my'/>
-                    <label for='fx'>Fx</label>
-                    <input onChange={handleCheckBox}type="checkbox" id="fz" name='fx'/>
-                    <label for='fx'>Fx</label>
-                </div>
-
-                <button onClick={selectedReactions} type='submit'>Generate Table</button>
-                
-            </form>
+  return (
+    <div>
+      <form action="submit">
+        <div class="column">
+          <input onChange={handleCheckBox} type="checkbox" id="fx" name="fx" />
+          <label for="fx">Fx</label>
+          <input onChange={handleCheckBox} type="checkbox" id="fy" name="fy" />
+          <label for="fy">Fz</label>
+          <input onChange={handleCheckBox} type="checkbox" id="fz" name="fz" />
+          <label for="fz">Fz</label>
         </div>
-    )
+        <div>
+          <input onChange={handleCheckBox} type="checkbox" id="mx" name="mx" />
+          <label for="mx">Mx</label>
+          <input onChange={handleCheckBox} type="checkbox" id="my" name="my" />
+          <label for="my">My</label>
+          <input onChange={handleCheckBox} type="checkbox" id="fz" name="fx" />
+          <label for="Mz">Mz</label>
+        </div>
+
+        <button onClick={selectedReactions} type="submit">
+          Generate Table
+        </button>
+      </form>
+    </div>
+  );
 }
 
-export default Reactions
+export default Reactions;
